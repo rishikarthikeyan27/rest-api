01866fd6de8d0482d026cf0c2e93057c459b1cf2
rishikarthikeyan27 <rishikar001@e.ntu.edu.sg>
2020-10-03 00:09:55 +0800

    added body-parser package

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+
 require('dotenv/config')
 
 const postsRoute = require('./routes/posts');
 
+app.use(bodyParser.json());
 app.use('/', postsRoute);
 //Routes
 //Connect to DB
--- routes/posts.js
+++ routes/posts.js
@@ -1,10 +1,21 @@
 const express = require('express');
 const router = express.Router();
+const Post = require('../models/Post');
+
 
 router.get('/', (req,res) => {
     res.send('Dont fear failure');
 })
 
+router.post('/', (req,res) => {
+    console.log(req.body);
+})
+router.patch('/', (req,res) =>{
+    console.log(req.body);
+})
+router.delete('/',(req,res) => {
+    console.log(req.body);
+})
 router.get('/why', (req,res) => {
     res.send('Becausem failure helps you grow');
 })
