46e2ba08fb187199b289334fd6fc8add3658628c
rishikarthikeyan27 <rishikar001@e.ntu.edu.sg>
2020-10-02 22:48:16 +0800

    connected to database

--- app.js
+++ app.js
@@ -1,9 +1,17 @@
 const express = require('express');
 const app = express();
-
+const mongoose = require('mongoose');
+require('dotenv/config')
 //Routes
 app.get('/', (req,res) => {
     res.send('We are getting better by the day bro');
 })
+
+//Connect to DB
+
+mongoose.connect(
+    process.env.DB_CONNECTION, 
+    {useNewUrlParser : true},
+    () => console.log('connected to DB!'))
 //Listen
 app.listen(3000);
